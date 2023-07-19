63eef351e68869cf25e77083b881c8b67f715f95
tars3017 <prgmpclh@gmail.com>
2023-07-19 13:12:49 +0800

    Put to server test.

--- script.js
+++ script.js
@@ -1,37 +1,115 @@
 
 class Comment {
-    constructor(name, text, courseName, prof) {
-        this.name = name;
+    constructor(text, courseName, prof, year, month, date) {
+        // this.name = name;
         this.text = text;
         this.courseName = courseName;
         this.prof = prof;
-        // this.date = date;
+        this.date = date;
+        this.month = month;
+        this.year = year;
         // this.time = time;
     }
 }
-let a = new Comment("XXX", "Good!", "AAA", "123");
-let b = new Comment("YYY", "Bad", "BBB", "456");
-let c = new Comment("ZZZ", "~~", "AAA", "123");
+let a = new Comment("Good!", "AAA", "123", "2021", "10", "10");
+let b = new Comment("Bad", "BBB", "456", "2022", "7", "3");
+let c = new Comment("~~", "AAA", "123", "2023", "7", "4");
 
 let comments = [a, b, c];
 
-$(window).ready(() => {
-    console.log('Document Loaded');
+function showCommentList() {
+    $('#commentList').empty();
+    $('#commentDetail').hide();
+    $('#commentList').show();
+
     let printed = [];
     comments.forEach(oneCom => {
         if (!printed.find(x => x === oneCom.courseName + oneCom.prof)) {
             let count = comments.filter(x=>x.courseName === oneCom.courseName).filter(x=>x.prof === oneCom.prof).length;
-            $('#commentList').append(`<div class="commentBar">
+            $('#commentList').append(`<div class="commentBar" courseName="${oneCom.courseName}" prof="${oneCom.prof}" onmouseover="this.style.cursor='pointer';">
     	課程名稱: ${oneCom.courseName}<br>
 	 	開課教授: ${oneCom.prof}<br>
    		有 ${count} 則評論
   	</div>`);
             printed.push(oneCom.courseName + oneCom.prof);
+
         }
     });
+    $('.commentBar').each(function() {
+        // $(this).css('overflow', 'auto');
+        // $(this).animate({height: 'auto'}, 1000);
+        $(this).click(function () {
+            let nowProf = $(this)[0].getAttribute("prof");
+            let nowCourseName = $(this)[0].getAttribute("courseName");
+            $('#commentList').hide();
+            showCommentDetail(nowProf, nowCourseName);
+        });
+    })
+}
 
+function showCommentDetail(nowProf, nowCourseName) {
+    $('#commentDetail').empty();
+    $('#commentDetail').show();
+    comments.forEach(oneCom=> {
+        if (oneCom.courseName === nowCourseName && nowProf === oneCom.prof) {
+            $('#commentDetail').append(`<div class="commentBar" onmouseover="this.style.cursor='default';">
+				<div class="commentTime">${oneCom.year}/${oneCom.month}/${oneCom.date}</div>
+				<div class="commentContent">
+                課程名稱: ${oneCom.courseName}<br>
+                開課教授: ${oneCom.prof}<br><br>
+                ${oneCom.text}
+ 				</div>
+            </div>`);
+        }
+    });
+    $('#commentDetail').append(
+        `<img src="https://briian.com/wp-content/uploads/2014/08/backbutton_0-200x198.png" 
+  			  alt="backButton"
+	   		  width="50" id="backButton"/>`
+    );
+
+    $('#backButton').click(function() {
+        $(this).hide();
+        showCommentList();
+    });
+}
+$(window).ready(() => {
+    console.log('Document Loaded');
+    showCommentList();
 });
 
-$('.commentBar').click(() => {
-    console.log('Find one bar');
+$('form').submit((event) => {
+    event.preventDefault();
+    const searchText = $('#searchInput').val().trim();
+
+    if (searchText === '') {
+        $('.commentBar').show();
+        return;
+    }
+
+    $('.commentBar').hide();
+    comments.forEach(oneCom => {
+        if (oneCom.courseName.includes(searchText)) {
+            const selector = `div.commentBar:contains("${oneCom.courseName}")`;
+            $(selector).show();
+        }
+    });
+});
+
+$('#submitButton').click(function() {
+    const nowCourseName = $('#courseInput').val().trim();
+    const nowProf = $('#professorInput').val().trim();
+    const nowText = $('#commentInput').val().trim();
+    if (nowCourseName === '' || nowProf === '' || nowText === '') {
+        alert('評論不完整!');
+        return ;
+    }
+    $('#courseInput').val('');
+    $('#professorInput').val('');
+    $('#commentInput').val('');
+
+    let nowDate = new Date();
+    let nowComment = new Comment(nowText, nowCourseName, nowProf, nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate());
+    comments.push(nowComment);
+    showCommentList();
 });
